c437809d46c3a7ca96f0ee6388c97ac266829bbd
Ambesh <ambeshpm@gmail.com>
2022-05-28 21:41:00 +0530

    updated .env file

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const app = express();
 const blogRoutes = require("./Routes/BlogRoutes");
 
+require('dotenv').config()
+const db= process.env.Db;
+
+
+
 //Middleware
 app.use(express.json());
 app.use("/post", blogRoutes);
